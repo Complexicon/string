f13588de2d56e4024ee97fb0e2ec6c326e4fbfa1
Complexicon <22222976+Complexicon@users.noreply.github.com>
2021-03-19 10:07:37 +0100

    string

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <str.h>
+#define println(x) printf("%s\n", x)
 
 int main(int argc, char const* argv[]) {
-	printf("%s\n", "Hello World!");
+	println("hello");
 	return 0;
 }
